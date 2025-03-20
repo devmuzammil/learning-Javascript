384f9f90bd413f24f542c0521a886fba24fc399a
Muzammil Hussain <muzammilhussainabro@gmail.com>
2025-03-20 18:25:00 +0500

    Added new project files

---
+++ Day 3/index.js
@@ -0,0 +1,19 @@
+function greet(name){
+    if(!name){
+        console.log('Hi there')
+    }
+    else
+    console.log(`Hello ${name}`);
+}
+
+greet();
+greet('Muzammil');
+greet('Muzammil');
+greet('Muzammil');
+function convertToFahrenheit(celsius){
+    fahrenheit=(celsius*9/5)+32;
+    console.log(fahrenheit);
+    return;
+}
+convertToFahrenheit(25);
+
---
+++ Day 7/Day 8/index.js
@@ -0,0 +1,6 @@
+const buttonElement=document.querySelector('.js-button');
+buttonElement.addEventListener('click',()=>{
+    console.log('click');
+})
+
+console.log([1,1,3].map(value=>value*2));
